d5040921e7c8e5ce4e4eee3cfe5de0417a7bf400
oliverhand5 <122156202+oliverhand5@users.noreply.github.com>
2023-01-10 21:05:07 +0000

    Add files via upload
    R files to produce boxplot charts of pathway stages split in all dimensions by stage, day/night, winter/non-winter, weekday/weekend, and including split of ED attendances with a diagnosed stroke / fall and all diagnosis groups respectively.

---
+++ stroke_fall_boxp_normal_5_8.R
@@ -0,0 +1,340 @@
+
+
+rm(list=ls())
+setwd("D:/r/chart_boxplot/split_day_time/")
+
+library(tidyverse)
+
+data_boxplot <- read_csv("boxp_normal_5-8.csv")
+
+
+## ALL
+# Fall Injuries Unknown
+ggplot(data_boxplot %>% 
+         filter(Amb_Complaint == 'Fall Injuries Unknown' ), aes(x = Cat, y = Vol)) +
+  geom_boxplot(outlier.colour='red') +
+  stat_summary(fun=mean, geom="point", shape=23, size=4) +
+  # theme_classic() +
+  xlab('Wait Type') +  # set x axis label, if removed the column heading name will be used.
+  ylab('Hours') +  # set y axis label, if removed the column heading name will be used
+  ggtitle('HWICB - Patients Waits For  Pathway Fall Injuries Unknown (Aug21 to Jul22)') +
+  scale_x_discrete(labels = function(Cat) str_wrap(Cat, width = 10)) +
+  facet_wrap(vars(County))
+ggsave('Fall_Injuries_Unknown_normal_all_5_8.png', height = 1400, width = 3000, units = "px") 
+
+
+# Fall Injuries Unknown
+ggplot(data_boxplot %>% 
+         filter(Amb_Complaint == 'Fall Non Injury' ), aes(x = Cat, y = Vol)) +
+  geom_boxplot(outlier.colour='red') +
+  stat_summary(fun=mean, geom="point", shape=23, size=4) +
+  # theme_classic() +
+  xlab('Wait Type') +  # set x axis label, if removed the column heading name will be used.
+  ylab('Hours') +  # set y axis label, if removed the column heading name will be used
+  ggtitle('HWICB - Patients Waits For  Pathway Fall Non Injury (Aug21 to Jul22)') +
+  scale_x_discrete(labels = function(Cat) str_wrap(Cat, width = 10)) +
+  facet_wrap(vars(County))
+ggsave('Fall_Non_Injury_normal_all_5_8.png', height = 1400, width = 3000, units = "px") 
+
+
+# Fall Injuries Unknown
+ggplot(data_boxplot %>% 
+         filter(Amb_Complaint == 'Stroke Neurological' ), aes(x = Cat, y = Vol)) +
+  geom_boxplot(outlier.colour='red') +
+  stat_summary(fun=mean, geom="point", shape=23, size=4) +
+  # theme_classic() +
+  xlab('Wait Type') +  # set x axis label, if removed the column heading name will be used.
+  ylab('Hours') +  # set y axis label, if removed the column heading name will be used
+  ggtitle('HWICB - Patients Waits For  Pathway Stroke Neurological (Aug21 to Jul22)') +
+  scale_x_discrete(labels = function(Cat) str_wrap(Cat, width = 10)) +
+  facet_wrap(vars(County))
+ggsave('Stroke_Neurological_normal_all_5_8.png', height = 1400, width = 3000, units = "px") 
+
+
+## -----------------------------------------------------------------------------
+## DAY
+
+
+ggplot(data_boxplot %>% 
+         filter(Amb_Complaint == 'Fall Injuries Unknown' ) %>% 
+       filter(is_day_night == 'day' ), aes(x = Cat, y = Vol)) +
+  geom_boxplot(outlier.colour='red') +
+  stat_summary(fun=mean, geom="point", shape=23, size=4) +
+  # theme_classic() +
+  xlab('Wait Type') +  # set x axis label, if removed the column heading name will be used.
+  ylab('Hours') +  # set y axis label, if removed the column heading name will be used
+  ggtitle('HWICB - Patients Waits For  Pathway Fall Injuries Unknown (Aug21 to Jul22) - Daytime only (08-18)') +
+  scale_x_discrete(labels = function(Cat) str_wrap(Cat, width = 10)) +
+  facet_wrap(vars(County))
+ggsave('Fall_Injuries_Unknown_normal_day_5_8.png', height = 1400, width = 3000, units = "px") 
+
+
+# Fall Injuries Unknown
+ggplot(data_boxplot %>% 
+         filter(Amb_Complaint == 'Fall Non Injury' ) %>% 
+         filter(is_day_night == 'day' ), aes(x = Cat, y = Vol)) +
+  geom_boxplot(outlier.colour='red') +
+  stat_summary(fun=mean, geom="point", shape=23, size=4) +
+  # theme_classic() +
+  xlab('Wait Type') +  # set x axis label, if removed the column heading name will be used.
+  ylab('Hours') +  # set y axis label, if removed the column heading name will be used
+  ggtitle('HWICB - Patients Waits For  Pathway Fall Non Injury (Aug21 to Jul22) - Daytime only (08-18)') +
+  scale_x_discrete(labels = function(Cat) str_wrap(Cat, width = 10)) +
+  facet_wrap(vars(County))
+ggsave('Fall_Non_Injury_normal_day_5_8.png', height = 1400, width = 3000, units = "px") 
+
+
+# Fall Injuries Unknown
+ggplot(data_boxplot %>% 
+         filter(Amb_Complaint == 'Stroke Neurological' ) %>% 
+         filter(is_day_night == 'day' ), aes(x = Cat, y = Vol)) +
+  geom_boxplot(outlier.colour='red') +
+  stat_summary(fun=mean, geom="point", shape=23, size=4) +
+  # theme_classic() +
+  xlab('Wait Type') +  # set x axis label, if removed the column heading name will be used.
+  ylab('Hours') +  # set y axis label, if removed the column heading name will be used
+  ggtitle('HWICB - Patients Waits For  Pathway Stroke Neurological (Aug21 to Jul22) - Daytime only (08-18)') +
+  scale_x_discrete(labels = function(Cat) str_wrap(Cat, width = 10)) +
+  facet_wrap(vars(County))
+ggsave('Stroke_Neurological_normal_day_5_8.png', height = 1400, width = 3000, units = "px") 
+
+
+## -----------------------------------------------------------------------------
+## NIGHT
+
+
+ggplot(data_boxplot %>% 
+         filter(Amb_Complaint == 'Fall Injuries Unknown' ) %>% 
+         filter(is_day_night == 'night' ), aes(x = Cat, y = Vol)) +
+  geom_boxplot(outlier.colour='red') +
+  stat_summary(fun=mean, geom="point", shape=23, size=4) +
+  # theme_classic() +
+  xlab('Wait Type') +  # set x axis label, if removed the column heading name will be used.
+  ylab('Hours') +  # set y axis label, if removed the column heading name will be used
+  ggtitle('HWICB - Patients Waits For  Pathway Fall Injuries Unknown (Aug21 to Jul22) - Nighttime only (18-08)') +
+  scale_x_discrete(labels = function(Cat) str_wrap(Cat, width = 10)) +
+  facet_wrap(vars(County))
+ggsave('Fall_Injuries_Unknown_normal_night_5_8.png', height = 1400, width = 3000, units = "px") 
+
+
+# Fall Injuries Unknown
+ggplot(data_boxplot %>% 
+         filter(Amb_Complaint == 'Fall Non Injury' ) %>% 
+         filter(is_day_night == 'night' ), aes(x = Cat, y = Vol)) +
+  geom_boxplot(outlier.colour='red') +
+  stat_summary(fun=mean, geom="point", shape=23, size=4) +
+  # theme_classic() +
+  xlab('Wait Type') +  # set x axis label, if removed the column heading name will be used.
+  ylab('Hours') +  # set y axis label, if removed the column heading name will be used
+  ggtitle('HWICB - Patients Waits For  Pathway Fall Non Injury (Aug21 to Jul22) - Nighttime only (18-08)') +
+  scale_x_discrete(labels = function(Cat) str_wrap(Cat, width = 10)) +
+  facet_wrap(vars(County))
+ggsave('Fall_Non_Injury_normal_night_5_8.png', height = 1400, width = 3000, units = "px") 
+
+
+# Fall Injuries Unknown
+ggplot(data_boxplot %>% 
+         filter(Amb_Complaint == 'Stroke Neurological' ) %>% 
+         filter(is_day_night == 'night' ), aes(x = Cat, y = Vol)) +
+  geom_boxplot(outlier.colour='red') +
+  stat_summary(fun=mean, geom="point", shape=23, size=4) +
+  # theme_classic() +
+  xlab('Wait Type') +  # set x axis label, if removed the column heading name will be used.
+  ylab('Hours') +  # set y axis label, if removed the column heading name will be used
+  ggtitle('HWICB - Patients Waits For  Pathway Stroke Neurological (Aug21 to Jul22) - Nighttime only (18-08)') +
+  scale_x_discrete(labels = function(Cat) str_wrap(Cat, width = 10)) +
+  facet_wrap(vars(County))
+ggsave('Stroke_Neurological_normal_night_5_8.png', height = 1400, width = 3000, units = "px") 
+
+
+## -----------------------------------------------------------------------------
+## weekday
+
+
+ggplot(data_boxplot %>% 
+         filter(Amb_Complaint == 'Fall Injuries Unknown' ) %>% 
+         filter(is_weekday == 'weekday' ), aes(x = Cat, y = Vol)) +
+  geom_boxplot(outlier.colour='red') +
+  stat_summary(fun=mean, geom="point", shape=23, size=4) +
+  # theme_classic() +
+  xlab('Wait Type') +  # set x axis label, if removed the column heading name will be used.
+  ylab('Hours') +  # set y axis label, if removed the column heading name will be used
+  ggtitle('HWICB - Patients Waits For  Pathway Fall Injuries Unknown (Aug21 to Jul22) - Weekday only') +
+  scale_x_discrete(labels = function(Cat) str_wrap(Cat, width = 10)) +
+  facet_wrap(vars(County))
+ggsave('Fall_Injuries_Unknown_normal_weekday_5_8.png', height = 1400, width = 3000, units = "px") 
+
+
+# Fall Injuries Unknown
+ggplot(data_boxplot %>% 
+         filter(Amb_Complaint == 'Fall Non Injury' ) %>% 
+         filter(is_weekday == 'weekday' ), aes(x = Cat, y = Vol)) +
+  geom_boxplot(outlier.colour='red') +
+  stat_summary(fun=mean, geom="point", shape=23, size=4) +
+  # theme_classic() +
+  xlab('Wait Type') +  # set x axis label, if removed the column heading name will be used.
+  ylab('Hours') +  # set y axis label, if removed the column heading name will be used
+  ggtitle('HWICB - Patients Waits For  Pathway Fall Non Injury (Aug21 to Jul22) - Weekday only') +
+  scale_x_discrete(labels = function(Cat) str_wrap(Cat, width = 10)) +
+  facet_wrap(vars(County))
+ggsave('Fall_Non_Injury_normal_weekday_5_8.png', height = 1400, width = 3000, units = "px") 
+
+
+# Fall Injuries Unknown
+ggplot(data_boxplot %>% 
+         filter(Amb_Complaint == 'Stroke Neurological' ) %>% 
+         filter(is_weekday == 'weekday' ), aes(x = Cat, y = Vol)) +
+  geom_boxplot(outlier.colour='red') +
+  stat_summary(fun=mean, geom="point", shape=23, size=4) +
+  # theme_classic() +
+  xlab('Wait Type') +  # set x axis label, if removed the column heading name will be used.
+  ylab('Hours') +  # set y axis label, if removed the column heading name will be used
+  ggtitle('HWICB - Patients Waits For  Pathway Stroke Neurological (Aug21 to Jul22) - Weekday only') +
+  scale_x_discrete(labels = function(Cat) str_wrap(Cat, width = 10)) +
+  facet_wrap(vars(County))
+ggsave('Stroke_Neurological_normal_weekday_5_8.png', height = 1400, width = 3000, units = "px")
+
+
+## -----------------------------------------------------------------------------
+## weekday
+
+
+ggplot(data_boxplot %>% 
+         filter(Amb_Complaint == 'Fall Injuries Unknown' ) %>% 
+         filter(is_weekday == 'weekend' ), aes(x = Cat, y = Vol)) +
+  geom_boxplot(outlier.colour='red') +
+  stat_summary(fun=mean, geom="point", shape=23, size=4) +
+  # theme_classic() +
+  xlab('Wait Type') +  # set x axis label, if removed the column heading name will be used.
+  ylab('Hours') +  # set y axis label, if removed the column heading name will be used
+  ggtitle('HWICB - Patients Waits For  Pathway Fall Injuries Unknown (Aug21 to Jul22) - Weekend only') +
+  scale_x_discrete(labels = function(Cat) str_wrap(Cat, width = 10)) +
+  facet_wrap(vars(County))
+ggsave('Fall_Injuries_Unknown_normal_weekend_5_8.png', height = 1400, width = 3000, units = "px") 
+
+
+# Fall Injuries Unknown
+ggplot(data_boxplot %>% 
+         filter(Amb_Complaint == 'Fall Non Injury' ) %>% 
+         filter(is_weekday == 'weekend' ), aes(x = Cat, y = Vol)) +
+  geom_boxplot(outlier.colour='red') +
+  stat_summary(fun=mean, geom="point", shape=23, size=4) +
+  # theme_classic() +
+  xlab('Wait Type') +  # set x axis label, if removed the column heading name will be used.
+  ylab('Hours') +  # set y axis label, if removed the column heading name will be used
+  ggtitle('HWICB - Patients Waits For  Pathway Fall Non Injury (Aug21 to Jul22) - Weekend only') +
+  scale_x_discrete(labels = function(Cat) str_wrap(Cat, width = 10)) +
+  facet_wrap(vars(County))
+ggsave('Fall_Non_Injury_normal_weekend_5_8.png', height = 1400, width = 3000, units = "px") 
+
+
+# Fall Injuries Unknown
+ggplot(data_boxplot %>% 
+         filter(Amb_Complaint == 'Stroke Neurological' ) %>% 
+         filter(is_weekday == 'weekend' ), aes(x = Cat, y = Vol)) +
+  geom_boxplot(outlier.colour='red') +
+  stat_summary(fun=mean, geom="point", shape=23, size=4) +
+  # theme_classic() +
+  xlab('Wait Type') +  # set x axis label, if removed the column heading name will be used.
+  ylab('Hours') +  # set y axis label, if removed the column heading name will be used
+  ggtitle('HWICB - Patients Waits For  Pathway Stroke Neurological (Aug21 to Jul22) - Weekend only') +
+  scale_x_discrete(labels = function(Cat) str_wrap(Cat, width = 10)) +
+  facet_wrap(vars(County))
+ggsave('Stroke_Neurological_normal_weekend_5_8.png', height = 1400, width = 3000, units = "px")
+
+
+## -----------------------------------------------------------------------------
+## winter
+
+
+ggplot(data_boxplot %>% 
+         filter(Amb_Complaint == 'Fall Injuries Unknown' ) %>% 
+         filter(is_winter == 'winter' ), aes(x = Cat, y = Vol)) +
+  geom_boxplot(outlier.colour='red') +
+  stat_summary(fun=mean, geom="point", shape=23, size=4) +
+  # theme_classic() +
+  xlab('Wait Type') +  # set x axis label, if removed the column heading name will be used.
+  ylab('Hours') +  # set y axis label, if removed the column heading name will be used
+  ggtitle('HWICB - Patients Waits For  Pathway Fall Injuries Unknown (Aug21 to Jul22) - Nov-Mar only') +
+  scale_x_discrete(labels = function(Cat) str_wrap(Cat, width = 10)) +
+  facet_wrap(vars(County))
+ggsave('Fall_Injuries_Unknown_normal_winter_5_8.png', height = 1400, width = 3000, units = "px") 
+
+
+# Fall Injuries Unknown
+ggplot(data_boxplot %>% 
+         filter(Amb_Complaint == 'Fall Non Injury' ) %>% 
+         filter(is_winter == 'winter' ), aes(x = Cat, y = Vol)) +
+  geom_boxplot(outlier.colour='red') +
+  stat_summary(fun=mean, geom="point", shape=23, size=4) +
+  # theme_classic() +
+  xlab('Wait Type') +  # set x axis label, if removed the column heading name will be used.
+  ylab('Hours') +  # set y axis label, if removed the column heading name will be used
+  ggtitle('HWICB - Patients Waits For  Pathway Fall Non Injury (Aug21 to Jul22) - Nov-Mar only') +
+  scale_x_discrete(labels = function(Cat) str_wrap(Cat, width = 10)) +
+  facet_wrap(vars(County))
+ggsave('Fall_Non_Injury_normal_winter_5_8.png', height = 1400, width = 3000, units = "px") 
+
+
+# Fall Injuries Unknown
+ggplot(data_boxplot %>% 
+         filter(Amb_Complaint == 'Stroke Neurological' ) %>% 
+         filter(is_winter == 'winter' ), aes(x = Cat, y = Vol)) +
+  geom_boxplot(outlier.colour='red') +
+  stat_summary(fun=mean, geom="point", shape=23, size=4) +
+  # theme_classic() +
+  xlab('Wait Type') +  # set x axis label, if removed the column heading name will be used.
+  ylab('Hours') +  # set y axis label, if removed the column heading name will be used
+  ggtitle('HWICB - Patients Waits For  Pathway Stroke Neurological (Aug21 to Jul22) - Nov-Mar only') +
+  scale_x_discrete(labels = function(Cat) str_wrap(Cat, width = 10)) +
+  facet_wrap(vars(County))
+ggsave('Stroke_Neurological_normal_winter_5_8.png', height = 1400, width = 3000, units = "px")
+
+
+## -----------------------------------------------------------------------------
+## winter
+
+
+ggplot(data_boxplot %>% 
+         filter(Amb_Complaint == 'Fall Injuries Unknown' ) %>% 
+         filter(is_winter == 'not_winter' ), aes(x = Cat, y = Vol)) +
+  geom_boxplot(outlier.colour='red') +
+  stat_summary(fun=mean, geom="point", shape=23, size=4) +
+  # theme_classic() +
+  xlab('Wait Type') +  # set x axis label, if removed the column heading name will be used.
+  ylab('Hours') +  # set y axis label, if removed the column heading name will be used
+  ggtitle('HWICB - Patients Waits For  Pathway Fall Injuries Unknown (Aug21 to Jul22) - Apr-Oct only') +
+  scale_x_discrete(labels = function(Cat) str_wrap(Cat, width = 10)) +
+  facet_wrap(vars(County))
+ggsave('Fall_Injuries_Unknown_normal_not_winter_5_8.png', height = 1400, width = 3000, units = "px") 
+
+
+# Fall Injuries Unknown
+ggplot(data_boxplot %>% 
+         filter(Amb_Complaint == 'Fall Non Injury' ) %>% 
+         filter(is_winter == 'not_winter' ), aes(x = Cat, y = Vol)) +
+  geom_boxplot(outlier.colour='red') +
+  stat_summary(fun=mean, geom="point", shape=23, size=4) +
+  # theme_classic() +
+  xlab('Wait Type') +  # set x axis label, if removed the column heading name will be used.
+  ylab('Hours') +  # set y axis label, if removed the column heading name will be used
+  ggtitle('HWICB - Patients Waits For  Pathway Fall Non Injury (Aug21 to Jul22) - Apr-Oct only') +
+  scale_x_discrete(labels = function(Cat) str_wrap(Cat, width = 10)) +
+  facet_wrap(vars(County))
+ggsave('Fall_Non_Injury_normal_not_winter_5_8.png', height = 1400, width = 3000, units = "px") 
+
+
+# Fall Injuries Unknown
+ggplot(data_boxplot %>% 
+         filter(Amb_Complaint == 'Stroke Neurological' ) %>% 
+         filter(is_winter == 'not_winter' ), aes(x = Cat, y = Vol)) +
+  geom_boxplot(outlier.colour='red') +
+  stat_summary(fun=mean, geom="point", shape=23, size=4) +
+  # theme_classic() +
+  xlab('Wait Type') +  # set x axis label, if removed the column heading name will be used.
+  ylab('Hours') +  # set y axis label, if removed the column heading name will be used
+  ggtitle('HWICB - Patients Waits For  Pathway Stroke Neurological (Aug21 to Jul22) - Apr-Oct only') +
+  scale_x_discrete(labels = function(Cat) str_wrap(Cat, width = 10)) +
+  facet_wrap(vars(County))
+ggsave('Stroke_Neurological_normal_not_winter_5_8.png', height = 1400, width = 3000, units = "px")
+
